19eb2e78b64d5d78ba126c73f1118549b6a5c23d
Maximiliano Alarcon <alarcon.maximiliano12345@gmail.com>
2026-03-07 15:46:47 -0300

    Update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return {
-        "project": "TransitPulse SF",
-        "status": "running"
-    }
+    return """
+    <html>
+        <head>
+            <title>TransitPulse SF</title>
+        </head>
+        <body>
+            <h1>TransitPulse SF 🚍</h1>
+            <p>API is running</p>
+        </body>
+    </html>
+    """
