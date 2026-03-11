640516ea3c25b5c26ab7297808a1b67ba706489a
Maximiliano Alarcon <alarcon.maximiliano12345@gmail.com>
2026-03-11 17:04:21 -0300

    Query

--- execute_query_postgis.py
+++ execute_query_postgis.py
@@ -25,64 +25,24 @@ def init_db(conn):
     cur = conn.cursor()
     # activar PostGIS
 
-    cur.execute("""
-    CREATE TABLE routes (
-        operator_id TEXT,
-        route_id TEXT,
-        agency_id TEXT,
-        route_short_name TEXT,
-        route_long_name TEXT,
-        route_desc TEXT,
-        route_type INT,
-        route_url TEXT,
-        route_color TEXT,
-        route_text_color TEXT,
-        PRIMARY KEY (operator_id, route_id)
-    );
-    """)
-
-    cur.execute("""
-    CREATE TABLE trips (
-        operator_id TEXT,
-        trip_id TEXT,
-        route_id TEXT,
-        service_id TEXT,
-        trip_headsign TEXT,
-        direction_id INT,
-        block_id TEXT,
-        shape_id TEXT,
-        trip_short_name TEXT,
-        bikes_allowed INT,
-        wheelchair_accessible INT,
-        PRIMARY KEY (operator_id, trip_id)
-    );
-    """)
-
-    cur.execute("""
-    CREATE TABLE stop_times (
-        operator_id TEXT,
-        trip_id TEXT,
-        arrival_time TEXT,
-        departure_time TEXT,
-        stop_id TEXT,
-        stop_sequence INT,
-        stop_headsign TEXT,
-        pickup_type INT,
-        drop_off_type INT,
-        shape_dist_traveled FLOAT,
-        timepoint INT,
-        PRIMARY KEY (operator_id, trip_id, stop_sequence)
-    );
-    """)
 
 
-    print("Cantidad de datos de stops")
+    print("Datos de routes")
     cur.execute("""
-    SELECT COUNT(*) FROM stops
+    SELECT * FROM routes LIMIT 10
     """)
     rows = cur.fetchall()  # trae todos los resultados
     for row in rows:
         print(row)
+
+    print("Cantidad de datos de routes")
+    cur.execute("""
+    SELECT COUNT(*) FROM routes
+    """)
+    rows = cur.fetchall()  # trae todos los resultados
+    for row in rows:
+        print(row)
+
 
     conn.commit()
     cur.close()
