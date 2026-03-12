e8f366fc945080f84bf5e5b34a40ef99ae3a231f
Maximiliano Alarcon <alarcon.maximiliano12345@gmail.com>
2026-03-11 21:27:05 -0300

    Test

--- execute_query_postgis.py
+++ execute_query_postgis.py
@@ -26,16 +26,86 @@ def init_db(conn):
     # activar PostGIS
 
     cur.execute("""
-CREATE INDEX IF NOT EXISTS stops_geom_idx
-ON stops
-USING GIST (geom);
+WITH params AS (
 
-ANALYZE;
+    SELECT
+        ST_SetSRID(ST_Point(-122.3816274, 37.6191145), 4326) AS origin_geom,
+        ST_SetSRID(ST_Point(-122.4120372, 37.7803603), 4326) AS dest_geom,
+        500 AS search_radius_m
 
-SELECT tablename, indexname
-FROM pg_indexes
-WHERE tablename IN ('stops','stop_times','routes')
-ORDER BY tablename;
+),
+
+-- paradas cerca del origen
+origin_stops AS (
+
+    SELECT s.stop_id, s.stop_name, s.geom
+    FROM stops s, params p
+    WHERE ST_DWithin(
+        s.geom::geography,
+        p.origin_geom::geography,
+        p.search_radius_m
+    )
+
+),
+
+-- paradas cerca del destino
+dest_stops AS (
+
+    SELECT s.stop_id, s.stop_name, s.geom
+    FROM stops s, params p
+    WHERE ST_DWithin(
+        s.geom::geography,
+        p.dest_geom::geography,
+        p.search_radius_m
+    )
+
+),
+
+-- viajes que pasan por origen
+origin_trips AS (
+
+    SELECT
+        st.trip_id,
+        st.stop_sequence,
+        st.stop_id
+    FROM stop_times st
+    JOIN origin_stops os
+    ON st.stop_id = os.stop_id
+
+),
+
+-- viajes que pasan por destino
+dest_trips AS (
+
+    SELECT
+        st.trip_id,
+        st.stop_sequence,
+        st.stop_id
+    FROM stop_times st
+    JOIN dest_stops ds
+    ON st.stop_id = ds.stop_id
+
+)
+
+SELECT
+    ot.trip_id,
+    os.stop_name AS origin_stop,
+    ds.stop_name AS destination_stop,
+    ot.stop_sequence AS origin_sequence,
+    dt.stop_sequence AS destination_sequence
+
+FROM origin_trips ot
+JOIN dest_trips dt
+ON ot.trip_id = dt.trip_id
+
+JOIN origin_stops os
+ON os.stop_id = ot.stop_id
+
+JOIN dest_stops ds
+ON ds.stop_id = dt.stop_id
+
+WHERE dt.stop_sequence > ot.stop_sequence
+LIMIT 20;
     """)
     rows = cur.fetchall()  # trae todos los resultados
     for row in rows:
