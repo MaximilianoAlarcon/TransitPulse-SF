8d68b48030fac0a8f1279899ec3cdf0aba4f13c8
Maximiliano Alarcon <alarcon.maximiliano12345@gmail.com>
2026-03-11 21:17:01 -0300

    Update execute_query_postgis.py

--- execute_query_postgis.py
+++ execute_query_postgis.py
@@ -26,54 +26,15 @@ def init_db(conn):
     # activar PostGIS
 
     cur.execute("""
--- =========================
--- LIMPIAR INDICE DUPLICADO
--- =========================
-
-DROP INDEX IF EXISTS idx_stops_geom;
-
--- =========================
--- INDICES NECESARIOS GTFS
--- =========================
-
--- indice espacial para buscar paradas cercanas
 CREATE INDEX IF NOT EXISTS stops_geom_idx
 ON stops
 USING GIST (geom);
 
--- buscar viajes por parada
-CREATE INDEX IF NOT EXISTS idx_stop_times_stop
-ON stop_times (stop_id);
-
--- joins por trip
-CREATE INDEX IF NOT EXISTS idx_stop_times_trip
-ON stop_times (trip_id);
-
--- orden de paradas dentro de un viaje
-CREATE INDEX IF NOT EXISTS idx_stop_times_trip_sequence
-ON stop_times (trip_id, stop_sequence);
-
--- join trips
-CREATE INDEX IF NOT EXISTS idx_trips_trip
-ON trips (trip_id);
-
--- join routes
-CREATE INDEX IF NOT EXISTS idx_routes_route
-ON routes (route_id);
-
--- =========================
--- ACTUALIZAR ESTADISTICAS
--- =========================
-
 ANALYZE;
-
--- =========================
--- VERIFICAR INDICES
--- =========================
 
 SELECT tablename, indexname
 FROM pg_indexes
-WHERE tablename IN ('stops','stop_times','trips','routes')
+WHERE tablename IN ('stops','stop_times','routes')
 ORDER BY tablename;
     """)
     rows = cur.fetchall()  # trae todos los resultados
@@ -81,29 +42,7 @@ ORDER BY tablename;
         print(row)
     
     
-    print("Indices")
-    cur.execute("""
-    SELECT indexname, indexdef
-FROM pg_indexes
-WHERE tablename = 'stop_times';
-    """)
-    rows = cur.fetchall()  # trae todos los resultados
-    for row in rows:
-        print(row)
 
-
-    print("Indices")
-    cur.execute("""
-    SELECT indexname, indexdef
-FROM pg_indexes
-WHERE tablename = 'stops';
-    """)
-    rows = cur.fetchall()  # trae todos los resultados
-    for row in rows:
-        print(row)
-
-    conn.commit()
-    cur.close()
 
 
 
