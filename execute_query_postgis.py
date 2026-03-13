169c4ffffb420f870932385cd0c528d8584f1099
KOAMaximilianoAlarcon <103137982+KOAMaximilianoAlarcon@users.noreply.github.com>
2026-03-13 20:11:57 -0300

    Query 2

--- execute_query_postgis.py
+++ execute_query_postgis.py
@@ -36,7 +36,7 @@ def init_db(conn):
 
     cur = conn.cursor()
     select(cur,"""
-    SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';
+    SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_schema = 'public' ORDER BY table_name, ordinal_position;
     """)
 
 
