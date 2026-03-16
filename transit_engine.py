d1db372a99747f3e550c9612dfce7c1deb72cbe8
KOAMaximilianoAlarcon <103137982+KOAMaximilianoAlarcon@users.noreply.github.com>
2026-03-16 14:34:51 -0300

    Update transit_engine.py

--- transit_engine.py
+++ transit_engine.py
@@ -25,7 +25,7 @@ pd.set_option('display.width', 200)         # ancho de la tabla en consola
 pd.set_option('display.max_rows', 50)      # mostrar hasta 50 filas
 
 
-def find_direct_trip(origin_coords, dest_coords, search_radius=2000):
+def find_direct_trip(origin_coords, dest_coords, search_radius=800):
 
     conn = psycopg2.connect(**DB_CONFIG)
     cur = conn.cursor()
